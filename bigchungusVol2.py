35073266582362e105b75108555f032d4f2f96c3
eerm123 <eero.jyrgenson00@gmail.com>
2023-12-20 01:22:41 +0200

    Uuendused tavalise game modeile

--- bigchungusVol2.py
+++ bigchungusVol2.py
@@ -10,11 +10,14 @@ pygame.init()
 LAIUS, KÕRGUS = 800, 600
 MUST = (0, 0, 0)
 VALGE = (255, 255, 255)
+KOLLANE = (255, 255, 0)
 PEALKIRI = "Hängmän"
 
 pygame.mixer.music.load("DokiMusic.mp3")
 pygame.mixer.music.set_volume(0.5)
 
+taustapilt_menüü = pygame.image.load("hangman_menüü.png")
+taustapilt_menüü = pygame.transform.scale(taustapilt_menüü, (LAIUS, KÕRGUS))
 taustapilt_mäng = pygame.image.load("Doki.png")
 taustapilt_mäng = pygame.transform.scale(taustapilt_mäng, (LAIUS,KÕRGUS))
 
@@ -44,14 +47,14 @@ hovered_choice = None  #Valin menüüvalikule hiirega liikumise jälgimise
 
 #Menüü tekstid ja nende asukohad ekraani peal
 tekstid = [
-    {"tekst": "Yo yo wälcome tu the gaem!", "x": 50, "y": 50},
+    {"tekst": "Tere tulemast Hanggangi!", "x": 50, "y": 50},
     {"tekst": "Valige üks võimalus:", "x": 50, "y": 140},
     {"tekst": "Alusta Hängmäni mängu", "x": 50, "y": 200},
-    {"tekst": "Hängman tupsu all", "x": 50, "y": 250},
+    {"tekst": "Hängman blitz (2min)", "x": 50, "y": 250},
     {"tekst": "Välju", "x": 50, "y": 300},
 ]
 menüü_font = pygame.font.Font(None, 36)
-teksti_värv = VALGE
+teksti_värv = KOLLANE
 
 #Funktsioon menüüvaliku hoveri jälgimiseks
 def kas_valik_hover(x, y, valik_laius, valik_kõrgus):
@@ -63,7 +66,9 @@ def kas_valik_hover(x, y, valik_laius, valik_kõrgus):
         return True
     return False
 
+##############################################################################################################################
 #Funktsioon uue suvalise sõna saamiseks
+
 def saa_suvaline_sõna():
     veebiaadress = "https://www.cl.ut.ee/ressursid/sagedused/tabel1.txt"
     urlfail = urlopen(veebiaadress)
@@ -83,7 +88,9 @@ def saa_suvaline_sõna():
         print("Ei leitud sobivaid sõnu")
         return ""
 
+##############################################################################################################################
 #Main menüü tsükkel
+    
 menüü_jookseb = True
 while menüü_jookseb:
     for sündmus in pygame.event.get():
@@ -117,18 +124,23 @@ while menüü_jookseb:
                     print("Varjatud sõna:", varjatud_sõna)
                     menüü_jookseb = False
                     valiku_näitaja = 0
-                elif valiktekst == "Hängman tupsu all":
+                elif valiktekst == "Hängman blitz":
                     alusta_mäng = True
+                    suvaline_sõna = saa_suvaline_sõna().upper()
+                    mängusõna = list(suvaline_sõna)  #Konverteerib sõna suurteks tähtedeks
+                    varjatud_sõna = ["_" if tähemärk.isalpha() else tähemärk for tähemärk in mängusõna]
+                    print("Varjatud sõna:", varjatud_sõna)
                     menüü_jookseb = False
                     valiku_näitaja = 1
                 elif valiktekst == "Välju":
                     menüü_jookseb = False
                     pygame.quit()
 
+##############################################################################################################################
     #Kui mäng pole veel alustatud, siis kuva menüüd
+                    
     if not alusta_mäng:
-        aken.fill(MUST)
-
+        aken.blit(taustapilt_menüü, (0, 0))
         for tekst_info in tekstid:
             tekst = menüü_font.render(tekst_info["tekst"], True, teksti_värv)
             aken.blit(tekst, (tekst_info["x"], tekst_info["y"]))
@@ -146,25 +158,9 @@ while menüü_jookseb:
 
     pygame.display.update()
 
-    #Kui mängu käima paned ja valisid hängman tupsu all, läheb mängul taimer käima
-    if alusta_mäng and valiku_näitaja == 1:
-        aken.fill(VALGE)
-        aken.blit(hangman_pildid[vigu], (65, 50))
-
-        countdown_duration = 10
-
-        for i in range(countdown_duration, 0, -1):
-            print(i)
-            time.sleep(1)
-
-        print("Aeg sai otsa!")
-        mängu_jookseb = False
-        pygame.quit()
-        sys.exit()
-
-    pygame.display.update()
-
+##############################################################################################################################
 #Täiendatud varjatud sõna loomine, kõik tähed nähtavad
+    
 varjatud_sõna = list("_" * len(suvaline_sõna))
 
 #Kõik tähed nähtavad
@@ -177,8 +173,12 @@ mängija_kaotas_varjund = menüü_font.render(mängija_kaotas_tekst, True, MUST)
 
 teksti_rect = mängija_kaotas_render.get_rect(topleft=(65, 300))
 varjundi_rect = pygame.Rect(teksti_rect.left - 2, teksti_rect.top - 2, teksti_rect.width + 4, teksti_rect.height + 4)
+mängu_kestus = 120
+algus_aeg = pygame.time.get_ticks()
 
+##############################################################################################################################
 #Põhimängu tsükkel
+
 mängu_jookseb = True
 while mängu_jookseb:
     for sündmus in pygame.event.get():
@@ -187,7 +187,9 @@ while mängu_jookseb:
             pygame.quit()
             sys.exit()
 
-        #Koodi lisamine, et haldada mängu loogikat
+##############################################################################################################################
+        #Esimese mängu mode(Tavaline hängmän)
+            
         if alusta_mäng and valiku_näitaja == 0:
             taustapilt = taustapilt_mäng
             menüü_reziim = False
@@ -197,7 +199,7 @@ while mängu_jookseb:
                 if sündmus.unicode.isalpha():  #Kontrollib, kas sisestatud sümbol on täht
                     arvatud_täht = sündmus.unicode.upper()
                     if arvatud_täht in kõik_tähed:
-                        kõik_tähed.remove(arvatud_täht)  ##Eemalda täht valikust
+                        kõik_tähed.remove(arvatud_täht)  #Eemalda täht valikust
                         if arvatud_täht in suvaline_sõna:
                             #Õige tähe arvamine, vaheta varjatud sõna vastavalt
                             for i, täht in enumerate(suvaline_sõna):
@@ -214,7 +216,7 @@ while mängu_jookseb:
                                 aken.blit(mängija_kaotas_varjund, varjundi_rect.topleft)
                                 aken.blit(mängija_kaotas_render, teksti_rect.topleft)
                                 pygame.display.update()
-                                pygame.time.delay(5000)  #Ootab 5000 ms (5 sekundit)
+                                pygame.time.delay(5000)  #Ootab (5 sekundit)
                                 pygame.quit()
                                 sys.exit()
 
@@ -254,6 +256,100 @@ while mängu_jookseb:
 
                 if peab_arvama_render.get_width() > LAIUS:
                     # Vähendab teksti suurust, et see mahuks ekraanile
+                    uus_font = pygame.font.Font(None, 22)
+                    peab_arvama_render = uus_font.render("Valikus olevad tähed: " + valikus_olevad_tähed, True, VALGE)
+                    peab_arvama_varjund = uus_font.render("Valikus olevad tähed: " + valikus_olevad_tähed, True, MUST)
+
+                teksti_rect = peab_arvama_render.get_rect(topleft=(65, 500))
+                varjundi_rect = pygame.Rect(teksti_rect.left - 2, teksti_rect.top - 2, teksti_rect.width + 4, teksti_rect.height + 4)
+
+                aken.blit(peab_arvama_varjund, varjundi_rect.topleft)
+                aken.blit(peab_arvama_render, teksti_rect.topleft)
+
+                #Kuvab sõna pikkuse
+                sõna_pikkus_render = menüü_font.render("Sõna pikkus: " + str(len(suvaline_sõna)), True, VALGE)
+                sõna_pikkus_varjund = menüü_font.render("Sõna pikkus: " + str(len(suvaline_sõna)), True, MUST)
+
+                teksti_rect = sõna_pikkus_render.get_rect(topleft=(65, 540))
+                varjundi_rect = pygame.Rect(teksti_rect.left - 2, teksti_rect.top - 2, teksti_rect.width + 4, teksti_rect.height + 4)
+
+                aken.blit(sõna_pikkus_varjund, varjundi_rect.topleft)
+                aken.blit(sõna_pikkus_render, teksti_rect.topleft)
+
+##############################################################################################################################
+##############################################################################################################################  
+        #Teise mängu mode(Hängmän tupsu all)
+                
+        elif alusta_mäng and valiku_näitaja == 1:
+            taustapilt = taustapilt_mäng
+            menüü_reziim = False
+
+            jooksnud_aeg = (pygame.time.get_ticks() - algus_aeg) // 1000
+
+            if jooksnud_aeg >= mängu_kestus:
+                print("Aeg sai otsa!")
+                pygame.quit()
+                sys.exit()
+
+            arvatud_tähed = set()
+            if sündmus.type == pygame.KEYDOWN:
+                if sündmus.unicode.isalpha():  #Kontrollib, kas sisestatud sümbol on täht
+                    arvatud_täht = sündmus.unicode.upper()
+                    if arvatud_täht in kõik_tähed:
+                        kõik_tähed.remove(arvatud_täht)  ##Eemalda täht valikust
+                        if arvatud_täht in suvaline_sõna:
+                            #Õige tähe arvamine, vaheta varjatud sõna vastavalt
+                            for i, täht in enumerate(suvaline_sõna):
+                                if täht == arvatud_täht:
+                                    varjatud_sõna[i] = arvatud_täht
+                            print("Õige täht:", arvatud_täht)
+                        else:
+                            vigu += 1
+                            print("Vale täht:", arvatud_täht)
+                            if vigu == 7:
+                                #Mängija kaotas
+                                aken.fill((20, 20, 20))
+                                aken.blit(hangman_pildid[vigu], (65, 50))
+                                aken.blit(mängija_kaotas_varjund, varjundi_rect.topleft)
+                                aken.blit(mängija_kaotas_render, teksti_rect.topleft)
+                                pygame.display.update()
+                                pygame.time.delay(5000)  #Ootab 5000 ms (5 sekundit)
+                                pygame.quit()
+                                sys.exit()
+
+            if not menüü_reziim:
+                aken.blit(taustapilt, (0, 0))
+
+                aken.blit(hangman_pildid[vigu], (65, 50))
+
+                #Kuvab varjatud sõna
+                varjatud_tekst = " ".join(varjatud_sõna)
+                varjatud_tekst_render = menüü_font.render(varjatud_tekst, True, VALGE)
+                varjatud_tekst_varjund = menüü_font.render(varjatud_tekst, True, MUST)
+
+                teksti_rect = varjatud_tekst_render.get_rect(topleft=(65, 420))
+                varjundi_rect = pygame.Rect(teksti_rect.left - 2, teksti_rect.top - 2, teksti_rect.width + 4, teksti_rect.height + 4)
+
+                aken.blit(varjatud_tekst_varjund, varjundi_rect.topleft)
+                aken.blit(varjatud_tekst_render, teksti_rect.topleft)
+
+                #Kuvab arvatud tähed
+                arvatud_tähed_render = menüü_font.render("Arvatud tähed: " + ", ".join(set(arvatud_tähed)), True, VALGE)
+                arvatud_tähed_varjund = menüü_font.render("Arvatud tähed: " + ", ".join(set(arvatud_tähed)), True, MUST)
+
+                teksti_rect = arvatud_tähed_render.get_rect(topleft=(65, 460))
+                varjundi_rect = pygame.Rect(teksti_rect.left - 2, teksti_rect.top - 2, teksti_rect.width + 4, teksti_rect.height + 4)
+
+                aken.blit(arvatud_tähed_varjund, varjundi_rect.topleft)
+                aken.blit(arvatud_tähed_render, teksti_rect.topleft)
+
+                #Kuvab tähti, mida peab arvama
+                valikus_olevad_tähed = ", ".join(kõik_tähed)
+                peab_arvama_render = menüü_font.render("Valikus olevad tähed: " + valikus_olevad_tähed, True, VALGE)
+                peab_arvama_varjund = menüü_font.render("Valikus olevad tähed: " + valikus_olevad_tähed, True, MUST)
+
+                if peab_arvama_render.get_width() > LAIUS:
+                    #Vähendab teksti suurust, et see mahuks ekraanile
                     uus_font = pygame.font.Font(None, 22)
                     peab_arvama_render = uus_font.render("Valikus olevad tähed: " + valikus_olevad_tähed, True, VALGE)
                     peab_arvama_varjund = uus_font.render("Valikus olevad tähed: " + valikus_olevad_tähed, True, MUST)
